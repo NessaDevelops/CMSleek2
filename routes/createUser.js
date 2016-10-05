24c4daeab306890d957b17ef55943af7567ff0e5
Jenessa Stabler <Jenessa Stabler>
2016-10-05 12:58:01 -0600

    User creation is working

--- routes/createUser.js
+++ routes/createUser.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var _ = require('underscore');
+var color = require('cli-color')
 var db = require('../public/scripts/database');
 var Users = db.users;
 
